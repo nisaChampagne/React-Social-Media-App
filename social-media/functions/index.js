97fe3dbe96164bb1d05a30fc8d5ed393580bc6e3
Nisa Champagne <nchampag23@gmail.com>
2020-04-25 10:39:36 -0400

    express usage

--- social-media/functions/index.js
+++ social-media/functions/index.js
@@ -1,5 +1,7 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const express = require('express')
+const app = express()
 
 admin.initializeApp();
 
@@ -10,7 +12,8 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
 });
 
-exports.getScreams = functions.https.onRequest((req, res)=> {
+
+app.get('/getscreams', (req, res)=> {
     if(req.method !== 'GET'){
         return res.status(400).json({error: 'Method not allowed'})
     }
@@ -28,10 +31,11 @@ exports.getScreams = functions.https.onRequest((req, res)=> {
         })
 })
 
-exports.createScreams = functions.https.onRequest((req, res)=> {
-    if(req.method !== 'POST'){
-        return res.status(400).json({error: 'Method not allowed'})
-    }
+app.post('/createscreams', (req,res)=> {
+    // if(req.method !== 'POST'){
+    //     return res.status(400).json({error: 'Method not allowed'})
+    // }
+    // express takes care of above!
 
     const newScream = {
         body: req.body.body,
@@ -50,3 +54,55 @@ exports.createScreams = functions.https.onRequest((req, res)=> {
         })
 
 })
+
+
+// exports.getScreams = functions.https.onRequest((req, res)=> {
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+
+//     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,DELETE,HEAD,PUT,OPTIONS');
+//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     if(req.method !== 'GET'){
+//         return res.status(400).json({error: 'Method not allowed'})
+//     }
+
+//     admin.firestore().collection('screams').get()
+//         .then(data => {
+//             let screams = [];
+//             data.forEach(doc => {
+//                 screams.push(doc.data())
+//             })
+//             return res.json(screams);
+//         })
+//         .catch(err => {
+//             console.error(err)
+//         })
+// })
+
+// exports.createScreams = functions.https.onRequest((req, res)=> {
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+
+//     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,DELETE,HEAD,PUT,OPTIONS');
+//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     if(req.method !== 'POST'){
+//         return res.status(400).json({error: 'Method not allowed'})
+//     }
+
+//     const newScream = {
+//         body: req.body.body,
+//         userHandle: req.body.userHandle,
+//         createdAt: admin.firestore.Timestamp.fromDate(new Date())
+//     }
+
+//     admin.firestore().collection('screams').add(newScream)
+//         .then((doc) => {
+//             res.json({message: `document ${doc.id} created successfully` })
+//             return res.json(newScream)
+//         })
+//         .catch((err) => {
+//             res.status(500).json({ error: 'something went wrong'});
+//             console.error(err)
+//         })
+
+// })
+
+exports.api = functions.https.onRequest(app);
--- social-media/src/App.js
+++ social-media/src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import logo from './logo.svg';
+import axios from 'axios'
 import './App.css';
 
 function App() {
 
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+      console.log('Effect')
+    axios.get('https://us-central1-socialmediaapp-9cf68.cloudfunctions.net/getScreams')
+      .then (res => {
+        const data = res.data;
+        console.log('GUUD', res.data);      
+            setData(data)
+      })
+      .catch(error => {
+        console.log("Oopsie ", error);
+      });
+
+  },[]);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hey</h1>
     </div>
   );
 }
